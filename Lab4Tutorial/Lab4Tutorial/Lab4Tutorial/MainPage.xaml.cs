25798b68a00b770b2646dc9151a1d8085e0ee1d5
Krzysztof Kubiak <krkubiak95@gmail.com>
2020-11-28 22:20:00 +0100

    Added next project and removed bin / obj catalogs

---
+++ Lab4Tutorial/Lab4Tutorial/Lab4Tutorial/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab4Tutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public const double MyBorderWidth = 3.5;
+        string translatedNumber;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
---
+++ Lab4Tutorial/Lab4Tutorial/Lab4Tutorial/MyPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab4Tutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+
+            var content = (StackLayout)this.Content;
+
+            this.loginButton.Clicked += (sender, e) => {
+                Debug.WriteLine("Clicked !");
+            };
+        }
+
+    }
+}
