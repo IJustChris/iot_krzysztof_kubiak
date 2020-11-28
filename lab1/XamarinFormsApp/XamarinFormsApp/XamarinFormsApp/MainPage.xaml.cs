5f5fd2842d8974fecaa0d16d999476f542758549
Krzysztof Kubiak <krkubiak95@gmail.com>
2020-11-28 19:19:06 +0100

    added simple UWP android app

---
+++ lab1/XamarinFormsApp/XamarinFormsApp/XamarinFormsApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private Person person = new Person();
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeEvents();
+        }
+
+        private void InitializeEvents()
+        {
+            entLastName.TextChanged += EntLastName_TextChanged;
+            entFirstName.TextChanged += EntFirstName_TextChanged;
+            entPhoneNumber.TextChanged += EntPhoneNumber_TextChanged;
+            btnCamera.Clicked += async (object sender, EventArgs e) => 
+            {
+                var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() {
+                
+                });
+
+                if (photo != null)
+                {
+                    imgPhoto.Source = ImageSource.FromStream(() => photo.GetStream());
+                }
+            };
+        }
+
+        private void EntPhoneNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.person.PhoneNumber = e.NewTextValue;
+        }
+
+        private void EntFirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.person.PhoneNumber = e.NewTextValue;
+        }
+
+        private void EntLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.person.PhoneNumber = e.NewTextValue;
+        }
+
+    }
+}
